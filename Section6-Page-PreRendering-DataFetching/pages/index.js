fe82b715ac0401defbc8a23a3bceb3c0a1ef3929
George Zaki <george.zaki@btech.com>
2024-03-20 09:49:17 +0200

    section 6 initial code

---
+++ Section6-Page-PreRendering-DataFetching/pages/index.js
@@ -0,0 +1,28 @@
+function HomePage(props) {
+
+  const { products } = props;
+
+  return (
+    <ul>
+      {products.map(product => (
+        <li key={product.id}>{product.title}</li>
+      ))}
+      
+    </ul>
+  );
+}
+
+export async function getStaticProps() {
+  // fetch data from an API
+  return {
+    props: {
+      products: [
+        { id: 'p1', title: 'Product 1' },
+        { id: 'p2', title: 'Product 2' },
+        { id: 'p3', title: 'Product 3' }
+      ]
+    }
+  };
+}
+
+export default HomePage;
