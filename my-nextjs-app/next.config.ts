0da6bd62935a9f4c45929070d251f39e401af684
George  Fikri <dev.george.emad@gmail.com>
2024-11-17 20:18:49 +0200

    Avoiding Hydration Mismatches in Next.js

--- my-nextjs-app/next.config.ts
+++ my-nextjs-app/next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  generateEtags: false,
+  headers: async () => [
+    {
+      source: '/(.*)',
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'no-store, max-age=0, must-revalidate',
+        },
+      ],
+    },
+  ],
 };
 
 export default nextConfig;
